445264d16a2a8bea78ed0901e837c06bc99dac16
beyourshine <handyzxc@gmail.com>
2017-12-29 12:03:56 +0700

    Chapter 5 done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title page_title = ""
-    @base_title = t "app.supt1"
+    @base_title = I18n.t "app.supt1"
     page_title == "" ? @base_title : page_title + " | " + @base_title
   end
 end
