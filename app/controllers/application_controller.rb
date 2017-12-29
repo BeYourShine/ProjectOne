dc07cfaf02d901e5b3828a9fc46300146bbc601d
beyourshine <handyzxc@gmail.com>
2017-12-29 12:03:56 +0700

    Chapter 7 done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
 
+  def change_language
+    cookies.permanent[:language] = params[:lang].to_sym
+    redirect_to root_url
+  end
+
   private
+
+  attr_reader :lang
+
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    @lang = cookies[:language].to_sym
+    I18n.locale =
+      if I18n.available_locales.include? lang
+        lang
+      else
+        I18n.default_locale
+      end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find_by id: params[:id]
+    return if user
+    flash[:success] = t "users.flash.no_user"
+    redirect_to root_url
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if user.save
+      flash[:success] = t "users.flash.success_create"
+      redirect_to user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  attr_reader :user
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
